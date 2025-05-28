68fa74556a9a20511d0f1bb41526f2933b335fa0
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-05-28 15:25:34 -0400

    Update 20-TS175-C08L01-PROJECT.sh

--- 20-TS175-C08L01-PROJECT.sh
+++ 20-TS175-C08L01-PROJECT.sh
@@ -1,12 +1,12 @@
-# get the lab and course identifiers
-profile_id=@lab.LabProfile.Id
-set +x
-export LAB_ID=$(curl --silent "https://labondemand.com/api/v3/labprofile?id=$profile_id" --header "api_key: $APIKEY" | jq --raw-output .Number)
-echo LAB_ID=\$\(curl --silent "https://labondemand.com/api/v3/labprofile?id=$profile_id" --header "api_key: xxxxxxxxxxxxxxxxxxxxxxx" \| jq --raw-output .Number\)
-echo $LAB_ID
-set -x
-export COURSE_ID=$(echo ${LAB_ID%%-*} | tr A-Z a-z)
+# set variable
+iApp=TS_iApp--station_v17.1.tmpl
 
-# download and run the nap-specific lab script
-curl --silent --output /tmp/common-tasks.sh https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/common-tasks.sh
-bash -x /tmp/common-tasks.sh
+# download config from GitHub, copy to bigip1 and load/merge onto bigip1
+curl --silent https://raw.githubusercontent.com/learnf5/ts/main/$iApp --output /home/student/Downloads/$iApp
+sudo scp /home/student/Downloads/$iApp 192.168.1.31:/shared/tmp
+sudo ssh 192.168.1.31 tmsh load /sys application template /shared/tmp/$iApp
+sudo ssh 192.168.1.31 tmsh save /sys config
+
+# run this lab's specific tasks saved on GitHub
+curl --silent --output /tmp/$LAB_ID.sh https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/$LAB_ID.sh
+source /tmp/$LAB_ID.sh
