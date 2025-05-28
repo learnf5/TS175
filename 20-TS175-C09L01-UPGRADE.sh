94ff8db304fa3950bd733f5681837293401c34a8
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-05-28 14:46:55 -0400

    Create 20-TS175-C09L01-UPGRADE.sh

---
+++ 20-TS175-C09L01-UPGRADE.sh
@@ -0,0 +1,3 @@
+# download and run the nap-specific lab script
+curl --silent --output /tmp/common-tasks.sh https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/common-tasks.sh
+bash -x /tmp/common-tasks.sh
