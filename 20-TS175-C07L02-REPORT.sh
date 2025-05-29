0091c07b6a42bfab98ac18698978aaa5045891c9
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-05-29 17:03:15 -0400

    Update 20-TS175-C07L02-REPORT.sh

--- 20-TS175-C07L02-REPORT.sh
+++ 20-TS175-C07L02-REPORT.sh
@@ -1,5 +1,5 @@
 # set variable
-iApp=TS_iApp--station_v17.1.tmpl
+iApp=TS_iApp--station_v17.5.tmpl
 
 # download config from GitHub, copy to bigip1 and load/merge onto bigip1
 curl --silent https://raw.githubusercontent.com/learnf5/ts/main/$iApp --output /home/student/Downloads/$iApp
