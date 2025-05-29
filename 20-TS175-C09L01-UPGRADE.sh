05a1bf035a48671e6ad8f2f4151da839e17e7820
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-05-29 17:00:06 -0400

    Update 20-TS175-C09L01-UPGRADE.sh

--- 20-TS175-C09L01-UPGRADE.sh
+++ 20-TS175-C09L01-UPGRADE.sh
@@ -1,5 +1,5 @@
 # set variable
-iApp=TS_iApp--station_v17.1.tmpl
+iApp=TS_iApp--station_v15.1.tmpl
 
 # download config from GitHub, copy to bigip1 and load/merge onto bigip1
 curl --silent https://raw.githubusercontent.com/learnf5/ts/main/$iApp --output /home/student/Downloads/$iApp
@@ -7,9 +7,6 @@ sudo scp /home/student/Downloads/$iApp 192.168.1.31:/shared/tmp
 sudo ssh 192.168.1.31 tmsh load /sys application template /shared/tmp/$iApp
 sudo ssh 192.168.1.31 tmsh save /sys config
 
-# download config from GitHub, copy to bigip1 and load/merge onto bigip1
-curl --silent https://raw.githubusercontent.com/learnf5/ts/main/TS_iApp--station_v15.1.tmpl --output /home/student/Downloads/iApp--station_v15.1.tmpl
-rm /home/student/Downloads/TS_iApp--station_v17.1.tmpl
 curl --silent https://raw.githubusercontent.com/learnf5/ts/main/BIGIP-16.1.5.2-0.0.5.iso.md5 --output /tmp/BIGIP-16.1.5.2-0.0.5.iso.md5
 curl --silent https://raw.githubusercontent.com/learnf5/ts/main/BIGIP-17.1.2.1-0.0.2.iso.md5 --output /tmp/BIGIP-17.1.2.1-0.0.2.iso.md5
 curl --silent https://raw.githubusercontent.com/learnf5/ts/main/BIGIP-17.5.0-0.0.15.iso.md5 --output /tmp/BIGIP-17.5.0-0.0.15.iso.md5
