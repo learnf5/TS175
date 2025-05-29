6d11f37524df7dae9ce0173953f7e470ca0660b2
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-05-29 17:01:48 -0400

    Update 20-TS175-C02L01-INITIAL.sh

--- 20-TS175-C02L01-INITIAL.sh
+++ 20-TS175-C02L01-INITIAL.sh
@@ -8,7 +8,7 @@ set -x
 export COURSE_ID=$(echo ${LAB_ID%%-*} | tr A-Z a-z)
 
 # set variable
-iApp=TS_iApp--station_v17.1.tmpl
+iApp=TS_iApp--station_v17.5.tmpl
 
 # download config from GitHub, copy to bigip1 and load/merge onto bigip1
 curl --silent https://raw.githubusercontent.com/learnf5/ts/main/$iApp --output /home/student/Downloads/$iApp
