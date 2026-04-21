e7f74b0c8c00896375cd6a54a8a7dec4ff454f25
djlamey <76072289+djlamey@users.noreply.github.com>
2026-04-21 09:38:33 -0400

    Update 30-TS175.sh
    add code to remove outdated md5 file from big-ip base state

--- 30-TS175.sh
+++ 30-TS175.sh
@@ -1,2 +1,5 @@
 # confirm bigip1 is active again
 for i in {1..12}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
+
+# remove outdated md5 file
+sudo ssh 192.168.1.31 rm /shared/images/bigip-17.1.1.1-0.0.2iso.md5
