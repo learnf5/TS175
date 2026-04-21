0ed51995406f65f77dd17b51009873b578f9119b
djlamey <76072289+djlamey@users.noreply.github.com>
2026-04-21 10:01:32 -0400

    Update 20-TS175-C03L01-IHEALTH.sh
    added code to download hotfix md5 file and copy over to the big-ip

--- 20-TS175-C03L01-IHEALTH.sh
+++ 20-TS175-C03L01-IHEALTH.sh
@@ -3,3 +3,6 @@ iApp=TS_iApp--station_v17.5.tmpl
 
 # download config from GitHub, copy to bigip1 and load/merge onto bigip1
 curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/$iApp --output /home/student/Downloads/$iApp
+
+curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/Hotfix-BIGIP-17.5.0.0.39.15-ENG.iso.md5 --output /tmp/Hotfix-BIGIP-17.5.0.0.39.15-ENG.iso.md5
+sudo scp /tmp/*.md5 192.168.1.31:/shared/images/
